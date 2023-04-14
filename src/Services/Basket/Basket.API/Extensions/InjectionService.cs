33e8750284ff2b3b4ae5f20cf12cc152e700c478
Olga Tiutiunnyk <olga.tiutiunnyk@quorumsoftware.com>
2023-04-14 13:31:52 +0200

    add rabbitmq messages

--- src/Services/Basket/Basket.API/Extensions/InjectionService.cs
+++ src/Services/Basket/Basket.API/Extensions/InjectionService.cs
@@ -1,6 +1,6 @@
 ﻿using Basket.API.GrpcServices;
 using Basket.API.Repositories;
-using Discount.Grpc.Protos;
+using MassTransit;
 using static Discount.Grpc.Protos.DiscountProtoService;
 
 namespace Basket.API.Extensions
@@ -14,6 +14,15 @@ namespace Basket.API.Extensions
                 o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 
             builder.Services.AddScoped<DiscountGrpcService>();
+
+            builder.Services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg0) =>
+                {
+                    cfg0.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            builder.Services.AddAutoMapper(typeof(Program));
 
             return builder;
         }
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -3,6 +3,9 @@ using Ordering.Application;
 using Ordering.Infrastructure;
 using Ordering.Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using MassTransit;
+using EventBus.Messages.Common;
+using Ordering.API.EventBusConsumer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+    config.UsingRabbitMq((ctx, cfg0) =>
+    {
+        cfg0.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        cfg0.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<BasketCheckoutConsumer>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
